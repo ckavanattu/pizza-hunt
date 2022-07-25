c0cc0203cffc68e393d6f94b1bf70c337aa58ee2
ckavanattu <chackojk@gmail.com>
2022-07-24 18:43:01 -0700

    added replies to comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,31 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
-const CommentSchema = newSchema({
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+const CommentSchema = new Schema(
+    {
     writtenBy: {
         type: String
     },
@@ -9,9 +34,23 @@ const CommentSchema = newSchema({
     },
     createdat: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-});
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+  });
 
 const Comment = model('Comment', CommentSchema);
 
